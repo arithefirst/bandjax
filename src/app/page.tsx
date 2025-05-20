33925d08d03ee32b872cb76bdfaa6bbb4fd4cc28
April Hall <ari@arithefirst.com>
2025-05-19 22:07:22 -0400

    style: Prettier format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,14 @@ export default function Home() {
   return (
     <main className="flex min-h-screen w-screen flex-col items-center">
       <div className="flex-1">I like boats</div>
-      <nav className="grid h-24 grid-cols-2 border-t w-full">
-        <div className="h-full w-full flex items-center border-r justify-center flex-col">
+      <nav className="grid h-24 w-full grid-cols-2 border-t">
+        <div className="flex h-full w-full flex-col items-center justify-center border-r">
           <House size={36} strokeWidth={1.5} />
-          <span className='font-bold'>Home</span>
+          <span className="font-bold">Home</span>
         </div>
-        <div className="h-full w-full flex items-center justify-center flex-col">
+        <div className="flex h-full w-full flex-col items-center justify-center">
           <Medal size={36} strokeWidth={1.5} />
-          <span className='font-bold'>Leaderboard</span>
+          <span className="font-bold">Leaderboard</span>
         </div>
       </nav>
     </main>
