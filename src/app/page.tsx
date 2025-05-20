ed206073bf524aca5401f0577ec932d02976aa28
April Hall <ari@arithefirst.com>
2025-05-19 22:06:42 -0400

    feat: setup ShadCN

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,19 @@
+import { House, Medal } from 'lucide-react';
 
 export default function Home() {
   return (
-    <p>Hello World</p>
+    <main className="flex min-h-screen w-screen flex-col items-center">
+      <div className="flex-1">I like boats</div>
+      <nav className="grid h-24 grid-cols-2 border-t w-full">
+        <div className="h-full w-full flex items-center border-r justify-center flex-col">
+          <House size={36} strokeWidth={1.5} />
+          <span className='font-bold'>Home</span>
+        </div>
+        <div className="h-full w-full flex items-center justify-center flex-col">
+          <Medal size={36} strokeWidth={1.5} />
+          <span className='font-bold'>Leaderboard</span>
+        </div>
+      </nav>
+    </main>
   );
 }
