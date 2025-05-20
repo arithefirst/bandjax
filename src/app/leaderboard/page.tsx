1119de2cdae7b7535919daf190de3e3ca7532a98
April Hall <ari@arithefirst.com>
2025-05-20 09:57:03 -0400

    feat: Leaderboard (mock data)

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -1,11 +1,72 @@
 import { Header } from '@/components/header';
+import { LeaderboardItem } from '@/components/lboardItem';
 import { Navbar } from '@/components/navbar';
+
+const leaderboardData = [
+  {
+    name: 'Bari Saxes',
+    image: 'https://michaelcreative.com/wp-content/uploads/sites/27/2017/09/RobHead.jpg',
+    score: 9850,
+  },
+  {
+    name: 'Alto Saxes',
+    image: 'fogettaboutit.png',
+    score: 8720,
+  },
+  {
+    name: 'Tenor Saxes',
+    image: 'fogettaboutit.png',
+    score: 7640,
+  },
+  {
+    name: 'Tubas',
+    image: 'fogettaboutit.png',
+    score: 6580,
+  },
+  {
+    name: 'Flutes',
+    image: 'fogettaboutit.png',
+    score: 5930,
+  },
+  {
+    name: 'Bari Saxes',
+    image: 'https://michaelcreative.com/wp-content/uploads/sites/27/2017/09/RobHead.jpg',
+    score: 9850,
+  },
+  {
+    name: 'Alto Saxes',
+    image: 'fogettaboutit.png',
+    score: 8720,
+  },
+  {
+    name: 'Tenor Saxes',
+    image: 'fogettaboutit.png',
+    score: 7640,
+  },
+  {
+    name: 'Tubas',
+    image: 'fogettaboutit.png',
+    score: 6580,
+  },
+  {
+    name: 'Flutes',
+    image: 'fogettaboutit.png',
+    score: 5930,
+  },
+];
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen w-screen flex-col items-center">
+    <main className="flex h-screen w-screen flex-col items-center">
       <Header />
-      <div className="flex flex-1 items-center justify-center">I like beautiful rocks</div>
+      <div className="w-full flex-1 overflow-scroll p-4">
+        <h1 className="mb-4 text-center text-2xl font-bold">Leaderboard</h1>
+        <div className="flex w-full flex-col gap-2">
+          {leaderboardData.map((item, i) => (
+            <LeaderboardItem {...item} key={`lbi-${i}`} place={i + 1} />
+          ))}
+        </div>
+      </div>
       <Navbar active="leaderboard" />
     </main>
   );
---
+++ src/components/lboardItem.tsx
@@ -0,0 +1,56 @@
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Trophy } from 'lucide-react';
+
+interface LeaderboardItemProps {
+  place: number;
+  name: string;
+  image: string;
+  score: number;
+}
+
+export function LeaderboardItem({ place, name, image, score }: LeaderboardItemProps) {
+  function Icon() {
+    switch (place) {
+      case 1:
+        return <Trophy className="mr-2 h-6 w-6 text-amber-600" />;
+      case 2:
+        return <Trophy className="mr-2 h-6 w-6 text-slate-500" />;
+      case 3:
+        return <Trophy className="mr-2 h-6 w-6 text-amber-800" />;
+      default:
+        return <span className="text-black-500 mr-2 text-center font-bold">{place}</span>;
+    }
+  }
+
+  function getColorClasses() {
+    switch (place) {
+      case 1:
+        return 'border-amber-500 bg-gradient-to-br from-amber-300 to-amber-400';
+      case 2:
+        return 'border-gray-500 from-gray-300 bg-gradient-to-br to-gray-400';
+      case 3:
+        return 'border-amber-700 from-amber-500 to-amber-600 bg-gradient-to-br';
+      default:
+        return 'bg-input';
+    }
+  }
+
+  return (
+    <div className={`flex w-full items-center rounded-lg border p-4 shadow-sm ${getColorClasses()} `}>
+      <div className="flex w-10 items-center justify-center">
+        <Icon />
+      </div>
+
+      <Avatar className="h-10 w-10 border border-white shadow-sm">
+        <AvatarImage src={image} alt={name} />
+        <AvatarFallback>{name.substring(0, 2).toUpperCase()}</AvatarFallback>
+      </Avatar>
+
+      <div className="ml-4 flex-1">
+        <p className="font-medium">{name}</p>
+      </div>
+
+      <div className="text-lg font-bold">{score.toLocaleString()}</div>
+    </div>
+  );
+}
