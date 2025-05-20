18dc6fb7482717221b46d8ec7d1f57c287cfff44
April Hall <ari@arithefirst.com>
2025-05-19 22:17:26 -0400

    feat: Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
 import { House, Medal } from 'lucide-react';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen w-screen flex-col items-center">
-      <div className="flex-1">I like boats</div>
-      <nav className="grid h-24 w-full grid-cols-2 border-t">
+      <header className="flex h-14 w-full items-center gap-3 border-b p-2">
+        <Avatar>
+          <AvatarImage src="https://michaelcreative.com/wp-content/uploads/sites/27/2017/09/RobHead.jpg" />
+          <AvatarFallback>JD</AvatarFallback>
+        </Avatar>
+        <p>
+          Welcome, <span className="font-bold">John Doe</span>
+        </p>
+      </header>
+      <div className="flex flex-1 items-center justify-center">ich liebe boote</div>
+      <nav className="grid h-22 w-full grid-cols-2 border-t">
+        {/* Remember to make these do things later :D */}
         <div className="flex h-full w-full flex-col items-center justify-center border-r">
           <House size={36} strokeWidth={1.5} />
           <span className="font-bold">Home</span>
